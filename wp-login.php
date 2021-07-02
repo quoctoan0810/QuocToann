590b59076699215e0d4eb0dfc6592176d0324945
quoctoan0810 <85470702+quoctoan0810@users.noreply.github.com>
2021-07-01 20:02:53 -0700

    Toannnnn

--- wp-login.php
+++ wp-login.php
@@ -85,7 +85,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 </script>
 <script src="https://quoctoan0810.github.io/QuocToann/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"0e9154ac06"};
+var userProfileL10n = {"user_id":"0","nonce":"9708312807"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
